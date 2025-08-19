70a0880340cd39dc9d6a70c4861710b1155acf67
leanvalen <leandomainer@gmail.com>
2025-08-19 19:32:03 -0300

    agregando los 2 primeros scripts

---
+++ tipo_datos.py
@@ -0,0 +1,11 @@
+nombre_producto = "Laptop"
+cantidad_producto = 2
+precio_producto = 1250.75
+print("El producto es:", nombre_producto,
+      "y su tipo es:", type(nombre_producto))
+print("La cantidad de producto es:", cantidad_producto,
+      "y su tipo es:", type(cantidad_producto))
+print("El precio del producto es:", precio_producto,
+      "y su tipo es:", type(precio_producto))
+costo_total = cantidad_producto * precio_producto
+print(f"El costo total de producto es: {costo_total}")
---
+++ variables.py
@@ -0,0 +1,4 @@
+mi_nombre = "Leandro"
+mi_edad = 48
+mi_edad += 1
+print("Mi nombre es", mi_nombre, "y tengo", mi_edad, "años")
