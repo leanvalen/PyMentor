b3b79cce398dbacc23873561536cae1c73e1f853
leanvalen <leandomainer@gmail.com>
2025-08-19 21:31:57 -0300

    Agregando operadores aritmeticos

---
+++ operadores_aritméticos.py
@@ -0,0 +1,10 @@
+ingresos = 2000
+gastos = 1350
+balance = ingresos - gastos
+print(
+    f"Tus ingresos fueron {ingresos}, tus gastos fueron {gastos} y el balance resultante es: {balance}")
+
+nombre = "leandro"
+año_nacimiento = 1976
+nombre_usuario = nombre + "-" + str(año_nacimiento)
+print(f"El nombre de usuario es {nombre_usuario}")
